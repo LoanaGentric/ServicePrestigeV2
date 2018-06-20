8026baf3c3ecd293cfaadc0c7da8950bbecfcfa5
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-06-21 00:15:15 +0200

    favIcon

--- index.php
+++ index.php
@@ -247,7 +247,7 @@
         <div class="title">
           <div class="titleDecoration">
             <img src="images/2white.png" alt="">
-            <h2>CONDITIONS</h2>
+            <h2>Les pré requis</h2>
             <img src="images/2white.png" alt="">
           </div>
         </div>
@@ -259,12 +259,12 @@
               Avoir un bon  niveau d’anglais et avoir passé avec succès notre test chauffeur.
             </p>
             <p class="text2">
-              Etre en possession d’un véhicule conforme avec notre charte qualité.
+              Être en possession d’un véhicule conforme à notre charte qualité.
             </p>
           </div>
           <div class="ll">
             <p class="text3">
-              Etre désireux d’offrir le meilleur des services à tous ses passagers et rester dans la droite ligne de conduite de Service Prestige.
+              Etre désireux d’offrir le meilleur des services à tous les passagers.
             </p>
             <img class="fond" src="images/fondCercle.png" alt="">
           </div>
@@ -273,7 +273,7 @@
               Respecter tous nos engagements qualité.
             </p>
             <p class="text5">
-              Etre titulaire d’une licence VTC et de fournir tous les documents administratifs règlementaires en bonne et due forme.
+              Etre titulaire d’une licence VTC et de fournir tous les documents administratifs règlementaires.
             </p>
           </div>
         </div>
